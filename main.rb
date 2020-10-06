b3670dd93e3117bdde2b00905cc09386f956d67f
fusakoo <obatafusako@gmail.com>
2020-10-06 16:29:53 -0700

    Migrating board logic

--- main.rb
+++ main.rb
@@ -2,12 +2,12 @@
 require_relative 'tictactoe'
 require_relative 'classes'
 
-def start_game
+def play_game
   puts "\n Let's play Tic Tac Toe!"
   puts "\n Use numbers 1-9 to pick a spot on the grid"
   puts " Player 1 will be 'X' and Player 2 will be 'O'"
-  sample_board = %w[1 2 3 4 5 6 7 8 9]
-  Board.display_board(sample_board)
+  sample_board = Board.new([*1..10])
+  sample_board.display_board
 
   puts "\n Alright. Player 1, what's your name?"
   @name1 = gets.chomp
@@ -18,21 +18,39 @@ def start_game
   player2 = Player.new(@name2)
 
   new_game = TicTacToe.new(player1, player2)
-  board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-  new_game.play(board)
+  new_board = Board.new
+  new_board.display_board
+
+  new_game.play(new_board.board)
+  replay_game(player1, player2)
 end
 
-def play_game
-  quit_playing = false
-  # continue_playing = false
+def new_game(player1, player2)
+  puts "\n Let's play Tic Tac Toe!"
+  puts "\n Use numbers 1-9 to pick a spot on the grid"
+  puts " Player 1 will be 'X' and Player 2 will be 'O'"
+  sample_board = Board.new([*1..10])
+  sample_board.display_board
 
-  until quit_playing
-    start_game
+  new_game = TicTacToe.new(player1, player2)
+  new_board = Board.new
+  new_game.play(new_board.board)
+end
 
-    puts "\n To stop playing, type 'quit'. Otherwise, hit 'Enter' to play again."
-    next unless gets.chomp == 'quit'
+def replay_game(player1, player2)
+  responded = false
 
-    quit_playing = true
+  until responded do
+    puts " Would you like to play again? (Y or N)"
+    response = gets.chomp.upcase
+
+    if response == 'Y'
+      new_game(player1, player2)
+    elsif response == 'N'
+      responded = true
+    else
+      puts " Sorry, invalid response. Please respond with 'Y' or 'N'."
+    end
   end
 end
 
